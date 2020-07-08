f7878178c3d68d20e4de55107d9dfcd4133ecd8f
Rachel G <63759734+rgoldstein23@users.noreply.github.com>
2020-07-08 10:16:23 -0600

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,68 @@
+## Coursera Getting and Cleaning Data Final
+
+library(dplyr)
+
+## Step 1
+
+# set working directory
+# Download the file
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl ="https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Course3.zip",method="curl")
+
+# Unzip the folder
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip("Course3.zip") 
+}
+
+# Read in files from folder and add descriptive column names
+
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("Code", "Activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "Subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "Subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
+# Merge train and test data and subject datasets
+X <- rbind(x_train, x_test)
+Y <- rbind(y_train, y_test)
+subject <- rbind(subject_train, subject_test)
+mergedData <- cbind(subject, Y, X)
+
+## Step 2: extract mean and standard deviation
+
+newdata <- mergedData %>% select(Subject,
+              code, contains("mean"), contains("std"))
+
+# Step 3: Assign activities to activity codes
+newdata$code <- activities[newdata$code, 2]
+
+# Step 4: Appropriately label dataset
+
+names(newdata)[2] = "Activity"
+names(newdata)[1] = "Subject"
+names(newdata)<-gsub("Acc", "Accelerometer", names(newdata))
+names(newdata)<-gsub("Gyro", "Gyroscope", names(newdata))
+names(newdata)<-gsub("BodyBody", "Body", names(newdata))
+names(newdata)<-gsub("Mag", "Magnitude", names(newdata))
+names(newdata)<-gsub("^t", "Time", names(newdata))
+names(newdata)<-gsub("^f", "Frequency", names(newdata))
+names(newdata)<-gsub("tBody", "TimeBody", names(newdata))
+names(newdata)<-gsub("-mean()", "Mean", names(newdata), ignore.case = TRUE)
+names(newdata)<-gsub("-std()", "STD", names(newdata), ignore.case = TRUE)
+names(newdata)<-gsub("-freq()", "Frequency", names(newdata), ignore.case = TRUE)
+names(newdata)<-gsub("angle", "Angle", names(newdata))
+names(newdata)<-gsub("gravity", "Gravity", names(newdata))
+
+# Step 5: create table with tidy data
+
+tidydata <-aggregate(. ~Subject + Activity, newdata, mean)
+tidydata <-tidydata[order(tidydata$Subject,tidydata$Activity),]
+write.table(tidydata, file = "tidydata.txt", row.name = FALSE)
+
+ 
+
+
